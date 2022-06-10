2f14f6999963639ead789bd586353de01af202b4
Kyle Reynolds <kbreyn06@gmail.com>
2022-06-10 12:16:12 -0500

    connected server to db

--- server.js
+++ server.js
@@ -1,5 +1,48 @@
-const mysql2 = require('mysql2');
+const mysql = require('mysql2');
 const inquirer = require('inquirer');
+const express = require('express');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      user: 'root',
+      password: 'Kr_9378a',
+      database: 'employee_db'
+    },
+    console.log('Connected to the employee database.')
+  );
 
 
 
+app.get('/', (req, res) => {
+    res.json({
+      message: 'Hello World'
+    });
+  });
+
+  db.query(`SELECT * FROM employee`, (err, rows) => {
+    console.log(rows);
+  });
+
+
+// Default response for any other request (Not Found)
+app.use((req, res) => {
+    res.status(404).end();
+  });
+
+
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
