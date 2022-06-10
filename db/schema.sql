0ec81e734945f7b1f7fd2bb4eb840df8268ffe59
Kyle Reynolds <kbreyn06@gmail.com>
2022-06-10 11:52:09 -0500

    db/schema/seeds

--- db/schema.sql
+++ db/schema.sql
@@ -1,14 +1,9 @@
-DROP DATABASE IF EXISTS employee_db;
-CREATE DATABASE employee_db;
-
-USE employee_db;
 
 CREATE TABLE department(
     id INT AUTO_INCREMENT NOT NULL,
     department_name VARCHAR(30) NOT NULL,
     PRIMARY KEY(id)
 );
-
 
 
 CREATE TABLE role(
@@ -25,6 +20,7 @@ CREATE TABLE employee(
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT NOT NULL,
-    manager_id INT,
+    manager_id VARCHAR(30),
     PRIMARY KEY(id)
 );
+
